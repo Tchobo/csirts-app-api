86026981c0581a8a24553108c02084be622c11dd
toussaintt <ricostous@gmail.com>
2024-09-03 13:15:31 +0100

    Correction de csirt.serializer, CsirtSerialier remplacer par CsirtDetailSerializer

--- app/csirt/views.py
+++ app/csirt/views.py
@@ -51,7 +51,7 @@ class CsirtViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         """Return the serializer class for request."""
         if self.action =='list':
-            return serializers.CsirtSerializer
+            return serializers.CsirtDetailSerializer
         elif self.action =='upload_image':
             return serializers.CsirtImageSerializer
         
